dd6736196987f2ec6eec4d7ea61fff23b82cee01
ulabrezina <ulabrezina@gmail.com>
2024-11-06 20:04:16 +0500

    Task4

--- Tyuiu.BrezinaYS.Sprint3.Task4.V29/Program.cs
+++ Tyuiu.BrezinaYS.Sprint3.Task4.V29/Program.cs
@@ -1,5 +1,5 @@
-﻿using Tyuiu.BrezinaYS.Sprint3.Task3.V4.Lib;
-namespace Tyuiu.BrezinaYS.Sprint3.Task3.V4
+﻿using Tyuiu.BrezinaYS.Sprint3.Task4.V29.Lib;
+namespace Tyuiu.BrezinaYS.Sprint3.Task4.V29
 {
     internal class Program
     {
@@ -10,31 +10,31 @@ namespace Tyuiu.BrezinaYS.Sprint3.Task3.V4
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
-            Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
-            Console.WriteLine("* Задание #3                                                              *");
-            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Тема: Использование опреаторов continue и break в цикле                 *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #29                                                              *");
             Console.WriteLine("* Выполнил: Брезина Юлия Сергеевна | ИИПб-24-2                            *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Используя цикл foreach удалить из строки все буквы j                    *");
-            Console.WriteLine("* в строке: plkjjdw cvjkl                                                 *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5,                        *");
+            Console.WriteLine("* вычислить значение функции y=cos(x)/x                                   *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-            string value = "plkjjdw cvjkl";
-            char item = 'j';
-            Console.WriteLine("Исходная строка: " + value);
-            Console.WriteLine("Исходная буква: " + item);
+            int startValue = -5;
+            int stopValue = 5;
+            double res = ds.Calculate(startValue, stopValue);
+
+            Console.WriteLine("Старт шага  = " + startValue);
+            Console.WriteLine("Конец шага  = " + stopValue);
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
             Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine("Полученная строка: " + ds.DeleteCharInString(value, item));
+            Console.WriteLine("Сумма ряда = " + res);
             Console.ReadKey();
-;
-
         }
     }
 }
