0f81f27f13e03786382c29e70e3ff38160a603ca
ulabrezina <ulabrezina@gmail.com>
2024-11-12 13:08:43 +0500

    Task7

---
+++ Tyuiu.BrezinaYS.Sprint3.Task7.V7.Lib/DataService.cs
@@ -0,0 +1,33 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.BrezinaYS.Sprint3.Task7.V7.Lib
+{
+    public class DataService : ISprint3Task7V7
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x == -2)
+                {
+                    y = Math.Round((Math.Cos(x) - 3 * x), 2);
+                    valueArray[count] = y;
+                    count++;
+
+                }
+                else
+                {
+                    y = Math.Round((Math.Cos(x) + (Math.Cos(x) /(x + 2)) - 3 * x), 2);
+                    valueArray[count] = y;
+                    count++;
+                }
+                
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.BrezinaYS.Sprint3.Task7.V7/Program.cs
@@ -0,0 +1,53 @@
+﻿using Tyuiu.BrezinaYS.Sprint3.Task7.V7.Lib;
+namespace Tyuiu.BrezinaYS.Sprint3.Task7.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Брезина Ю. С. | ИИПб-24-2";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Брезина Юлия Сергеевна | ИИПб-24-2                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, выводит таблицу значений                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага  = " + startValue);
+            Console.WriteLine("Конец шага  = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+            double[] valueArray;
+            valueArray = new double[len];
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|    X     |    f(x)  |");
+            Console.WriteLine("+----------+----------+");
+            for (int i = 0; i <= len-1; i++)
+            {
+                Console.WriteLine("|{0,5:d}     |  {1,5:f2}   |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+----------+");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
